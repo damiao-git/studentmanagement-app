d74aabec799f1130af6d84c65d5308a1041853f6
damigaos <damiao.jose@hotmail.com>
2024-12-26 21:59:36 -0300

    Student part1

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index(){
+
+    }
+    public function show($id){
+
+    }
+    public function create(){
+
+    }
+    public function store(Request $request){
+
+    }
+    public function edit(){
+
+    }
+    public function update(Request $request, $id){
+
+    }
+    public function delete($id){
+
+    }
+}
